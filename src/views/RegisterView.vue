372dbb4e96b5ece6b6dcfffd96b1f7bd1b2da607
repoleved08 <geekbii08@gmail.com>
2024-09-28 19:46:44 +0300

    fixed auth router-link

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="w-full max-w-sm mx-auto my-3 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        class="w-full max-w-sm mx-auto my-5 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         <form class="space-y-6" action="#">
             <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign up to our platform</h5>
             <div>
@@ -27,22 +27,11 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                     required />
             </div>
-            <div class="flex items-start">
-                <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                        <input id="remember" type="checkbox" value=""
-                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                            required />
-                    </div>
-                    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
-                        me</label>
-                </div>
-                <a href="#" class="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-            </div>
+            
             <button type="submit"
                 class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register your account</button>
             <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                already registered? <router-link :to="{name 'login'}" class="text-blue-700 hover:underline dark:text-blue-500">Login to your account</router-link>
+                already registered? <router-link :to="{name: 'login'}" class="text-blue-700 hover:underline dark:text-blue-500">Login to your account</router-link>
             </div>
         </form>
     </div>
